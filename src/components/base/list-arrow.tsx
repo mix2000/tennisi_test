0a990896d56f67268d15a56cf6a85dd5ad837f11
trick <tricking.mix@gmail.com>
2022-04-14 14:32:51 +0300

    Добавлены два блока. Пакет для обработки клика вне. useMemo для меморизации иконок.

--- src/components/base/list-arrow.tsx
+++ src/components/base/list-arrow.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export const ListArrow = ():JSX.Element => {
-    return (
-        <div className={'list-arrow'}>
-            <svg width="8" height="5" viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7 1L4 4L1 1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-        </div>
-    );
-};
+export const ListArrow = (): JSX.Element => (
+    <div className={'list-arrow'}>
+        <svg width="8" height="5" viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7 1L4 4L1 1" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+    </div>
+);
